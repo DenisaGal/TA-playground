5cd32b1b410e57ffbdf3623c759e349ae1b4d189
Deni <denisagal09@yahoo.com>
2020-05-16 13:51:36 +0300

    Tried added log in

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/login.fxml"));
+        primaryStage.setTitle("FIS - Student Manager");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
+
+        public static void main(String[] args) {
+        launch(args);
+    }
+
+}
